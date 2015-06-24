fb967a48680b94d1f3d0f45f6a114c7b3b1241ba
s2quake <s2quake@ntreev.com>
2015-06-24 17:14:52 +0900

    누락 파일

---
+++ lib/WinformGridHost/DefaultFontProvider.cs
@@ -0,0 +1,18 @@
+﻿using Ntreev.Library.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ntreev.Windows.Forms.Grid
+{
+    class DefaultFontProvider : IDefaultFontProvider
+    {
+        public GrFont GetFont()
+        {
+            return new GrFontDC(Control.DefaultFont.ToHfont());
+        }
+    }
+}
