27dd823922a2f92bd2ad06c5ab16f5c09a1b8df0
s2quake <s2quake@ntreev.com>
2015-11-10 15:57:48 +0900

    2.0 로그 정리 .net migration 테스트랑 섞여서 정리했음.

--- lib/Ntreev.Library.Grid/_GrItemMouseEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Ntreev.Library.Grid
-{
-    public delegate void ItemMouseEventHandler(object sender, GrItemMouseEventArgs e);
-}
